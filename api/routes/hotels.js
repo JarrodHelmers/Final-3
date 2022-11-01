4cf39e8e4fa2ab9bfcd9c7a95870626a65767c95
Jarrod Macbook <jarrodhelmers@Jarrods-Air.attlocal.net>
2022-10-31 19:32:39 -0500

    added error.js

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -45,12 +45,12 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 });
 
