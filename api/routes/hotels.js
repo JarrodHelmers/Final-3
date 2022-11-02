0faf388938561331c5c95d916d296f44408e30d8
Jarrod Macbook <jarrodhelmers@Jarrods-Air.attlocal.net>
2022-11-01 19:48:43 -0500

    Done, just have to redo my mongo data

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,57 +1,29 @@
 import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
+router.post("/", verifyAdmin, createHotel);
 
-  try {
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", verifyAdmin, updateHotel);
 
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", verifyAdmin, deleteHotel);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted!");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/find/:id", getHotel);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.find(req.params.id);
-    res.status(200).json(hotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/", async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
